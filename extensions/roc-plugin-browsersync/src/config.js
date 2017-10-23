5ff679a0afb18b7cf5805181f4489a4b65610298
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2017-10-23 14:58:12 +0100

    Fixed based on code review

--- extensions/roc-plugin-browsersync/src/config.js
+++ extensions/roc-plugin-browsersync/src/config.js
@@ -1,4 +1,4 @@
-import { isBoolean, isInteger, isString, oneOf, required } from 'roc/validators';
+import { isBoolean, isInteger, isString, oneOf, required, notEmpty } from 'roc/validators';
 
 export const config = {
     settings: {
@@ -7,7 +7,7 @@ export const config = {
                 options: {
                     __raw: {},
                     open: true,
-                    host: '',
+                    host: null,
                     port: 3030,
                 },
                 enabled: true,
@@ -33,11 +33,11 @@ export const meta = {
                         description: 'Decide which URL to open automatically when Browsersync starts. ' +
                             'Defaults to "local" if none set. Can be true, local, external, ' +
                             'ui, ui-external, tunnel or false.',
-                        validator: required(oneOf(isString, isBoolean)),
+                        validator: required(oneOf(/^(external|ui|ui-external|tunnel)$/, isBoolean)),
                     },
                     host: {
-                        description: 'The host that Browsersync should use.',
-                        validator: isString,
+                        description: 'The host that Browsersync should use, will be automatic by default.',
+                        validator: notEmpty(isString),
                     },
                     port: {
                         description: 'The port that Browsersync should start on, should be a range of at least 2.',
